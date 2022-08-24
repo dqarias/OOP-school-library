0fd9212716d7b34487e300c26026a9548c153be8
dqarias <dino.quispea@gmail.com>
2022-08-24 15:40:25 -0500

    Update person class to inherit from Nameable

--- person.rb
+++ person.rb
@@ -1,8 +1,13 @@
-class Person
+require_relative('nameable')
+require_relative('capitalize_decorator')
+require_relative('trimmer_decorator')
+
+class Person < Nameable
   attr_reader :id
   attr_accessor :name, :age
 
   def initialize(age, name = 'Unknown', parent_permission: true)
+    super()
     @id = Random.rand(1...10_000)
     @age = age
     @name = name
@@ -25,8 +30,19 @@ class Person
       false
     end
   end
+
+  def correct_name
+    @name
+  end
 end
 
 person1 = Person.new(16, 'Ronald', parent_permission: false)
 p person1
 puts person1.can_use_services?
+
+person = Person.new(22, 'maximilianus')
+puts person.correct_name
+puts capitalized_person = CapitalizeDecorator.new(person)
+puts capitalized_person.correct_name
+puts capitalized_trimmed_person = TrimmerDecorator.new(capitalized_person)
+puts capitalized_trimmed_person.correct_name
